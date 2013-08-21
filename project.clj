c323610d6c6943505b9474c0d27064d23f617f50
Ryan Brush <rbrush@cerner.com>
2013-08-20 22:38:30 -0500

    Refactor source directories to prep for Java inclusion.

--- project.clj
+++ project.clj
@@ -5,4 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.codehaus.jsr166-mirror/jsr166y "1.7.0"]]
+  :source-paths ["src/main/clojure"]
+  :test-paths ["src/test/clojure"]
+  :java-source-paths ["src/main/java"]
   :aot :all)
