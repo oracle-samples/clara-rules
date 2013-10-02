6a40afc70a7fd5983b04dc3f30dcfcdcdcffc34e
Ryan Brush <rbrush@cerner.com>
2013-10-01 23:13:04 -0500

    Increment to next development version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.toomuchcode/clara-rules "0.1.1"
+(defproject org.toomuchcode/clara-rules "0.1.2-SNAPSHOT"
   :description "Clara Rules Engine"
   :url "http://rbrush.github.io/clara-rules/"
   :license {:name "Eclipse Public License"
