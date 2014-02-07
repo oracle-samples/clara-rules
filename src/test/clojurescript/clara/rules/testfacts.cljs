06b13dcfd57c95da08f408dd39cf1994544f1171
Ryan Brush <rbrush@cerner.com>
2014-02-06 20:02:18 -0600

    Add test facts to ClojureScript

---
+++ src/test/clojurescript/clara/rules/testfacts.cljs
@@ -0,0 +1,16 @@
+(ns clara.rules.testfacts
+  "This namespace exists primary for testing purposes, working around the fact that we cannot AOT compile test classes. This should be moved to the tests once a workaround for this is solved.")
+
+;; Reflection against records requires them to be compiled AOT, so we temporarily
+;; place them here as leiningen won't AOT compile test resources.
+(defrecord Temperature [temperature location])
+(defrecord WindSpeed [windspeed location])
+(defrecord Cold [temperature])
+(defrecord ColdAndWindy [temperature windspeed])
+(defrecord LousyWeather [])
+
+;; Test facts for chained rules.
+(defrecord First [])
+(defrecord Second [])
+(defrecord Third [])
+(defrecord Fourth [])
