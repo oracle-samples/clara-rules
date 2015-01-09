f304a27c49ec4835938a90774d4b56eef0227a7f
Ryan Brush <rbrush@gmail.com>
2015-01-09 13:21:33 -0600

    Prepare for 0.9 development.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.toomuchcode/clara-rules "0.8.2"
+(defproject org.toomuchcode/clara-rules "0.9.0-SNAPSHOT"
   :description "Clara Rules Engine"
   :url "http://rbrush.github.io/clara-rules/"
   :license {:name "Eclipse Public License"
