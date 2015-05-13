ab7735bfeb84b657b7c3c516c27b4fa2ce7625cc
Rodriguez,Mike <mike.rodriguez@cerner.com>
2015-05-13 16:01:48 -0500

    - Comparing arbitrary conditions in clara.rules.compiler (i.e. gen-compare) has potentially to throw NPE.  Implemented handling and reproduction case.

--- src/test/clojure/clara/test_rules.clj
+++ src/test/clojure/clara/test_rules.clj
@@ -2485,3 +2485,18 @@
 
     (is (= [{:?s "abc"}]
            (query session find-string-substring)))))
+
+(deftest test-condition-comparison-nil-safe
+  (let [q (dsl/parse-query []
+                           ;; Make two conditions that are very similar, but differ
+                           ;; where a nil will be compared to something else.
+                           [[(accumulate :reduce-fn (fn [x y] nil)) :from [Temperature]]
+                            [(accumulate :reduce-fn (fn [x y] 10)) :from [Temperature]]])
+        s (mk-session [q])]
+
+    ;; Mostly just ensuring the rulebase was compiled successfully.
+    (is (== 3
+            (-> s
+                .rulebase
+                :id-to-node
+                count)))))
