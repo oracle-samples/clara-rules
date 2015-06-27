55ab884c5a5c330fbc971fff1eaafd85e970757c
Luc Préfontaine <lprefontaine@softaddicts.ca>
2015-06-27 00:11:59 +0000

    Moved clj tests in its own profile, added tools.trace in dependencies

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/rbrush/clara-rules"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-RC2"] 
+  :dependencies [[org.clojure/clojure "1.7.0-RC2"]
+                 [org.clojure/tools.trace "0.7.8"]                  
                  [org.codehaus.jsr166-mirror/jsr166y "1.7.0"]
                  [org.clojure/clojurescript "0.0-3308"]
                  [prismatic/schema "0.4.3"]
@@ -26,15 +27,15 @@
                         :jar true
                         :compiler {:pretty-print true
                                    :output-to "target/js/clara.js"
-                                   :optimizations :whitespace}}
-
-                       ;; Build for unit tests.
-                       :test {:source-paths ["src/main/clojurescript" "src/test/clojurescript" "test"]
-                              :notify-command ["phantomjs" "phantom/unit-test.js" "phantom/unit-test.html"]
-                              :compiler {:output-to "target/cljs-test/testable.js"
-                                         :pretty-print true
-                                         :optimizations :whitespace}}}}
+                                   :optimizations :whitespace}}}}
   :profiles {:dev {:dependencies [[cljsbuild "1.0.6"]]}
+             :test {:cljsbuild {:builds 
+                                {:test {:source-paths ["src/main/clojurescript" "src/test/clojurescript" "test"]
+                                        :notify-command ["phantomjs" "phantom/unit-test.js" "phantom/unit-test.html"]
+                                        :compiler {:output-to "target/cljs-test/testable.js"
+                                                   :pretty-print true
+                                                   :optimizations :whitespace}}}}}
+             
              :aot {:aliases {"check" ["do" "clean," "compile"]}
                    :resource-paths ["/tmp/clara.rules/target/js/out"]
                    :target-path "/tmp/clara.rules/target/%s"
