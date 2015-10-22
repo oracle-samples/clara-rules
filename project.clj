8edbf2fe327f2056d23dcbf5318f923966ef2797
Ryan Brush <rbrush@gmail.com>
2015-10-21 22:57:53 -0500

    Update versions and add the javadoc plugin.

--- project.clj
+++ project.clj
@@ -4,17 +4,20 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.48" :scope "provided"]
-                 [prismatic/schema "0.4.3"]
-                 [dorothy "0.0.4"]
-                 [hiccup "1.0.4"]]
+                 [org.clojure/clojurescript "1.7.145" :scope "provided"]
+                 [prismatic/schema "1.0.1"]
+                 [dorothy "0.0.6"]
+                 [hiccup "1.0.5"]]
   :plugins [[codox "0.8.10"]
+            [lein-javadoc "0.2.0"]
             [lein-cljsbuild "1.1.0"]]
   :codox {:exclude [clara.other-ruleset clara.sample-ruleset clara.test-java
                     clara.test-rules clara.rules.memory clara.test-accumulators
                     clara.rules.testfacts clara.rules.java clara.rules.engine
                     clara.rules.compiler clara.rules.platform
                     clara.test-durability clara.tools.test-inspect clara.tools.test-tracing]}
+  :javadoc-opts {:package-names "clara.rules"}
+
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure" "src/test/common"]
   :java-source-paths ["src/main/java"]
