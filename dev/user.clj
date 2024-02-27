271fa4f1c96ab55beb9f611ac98287e91cae416c
Jose Gomez <jose.gomez@gateless.com>
2024-02-27 12:23:04 -0600

    chore: add useful user ns scribles

--- dev/user.clj
+++ dev/user.clj
@@ -3,7 +3,7 @@
                                     quick-benchmark] :as crit]
             [clara.rules.platform :refer [compute-for]]
             [clojure.core.async :refer [go timeout <!]]
-            [clara.rules :refer [mk-session]]
+            [clara.rules :refer [defrule mk-session clear-ns-productions!]]
             [clara.rules.compiler :as com]
             [clojure.core.cache.wrapped :as cache]
             [schema.core :as sc]
@@ -41,7 +41,8 @@
 (defmacro mk-rules
   [n]
   (let [facts (for [n (range n)]
-                {:fact-type {:t (symbol (format "FactType%s" n))
+                {:decl-name (symbol (format "rule-%s" n))
+                 :fact-type {:t (symbol (format "FactType%s" n))
                              :c (symbol (format "%s.FactType%s" (ns-name *ns*) n))}
                  :fact-record {:t (symbol (format "FactRecord%s" n))
                                :c (symbol (format "%s.FactRecord%s" (ns-name *ns*) n))}})
@@ -53,14 +54,25 @@
                               :constraints []}
                              {:type ~(:c fact-record)
                               :constraints []}]
-                       :rhs '(println (str "class:" ~n ~fact-type ~fact-record))))]
-    `(vector
-      ~@fact-rules)))
+                       :rhs '(println (str "class:" ~n ~fact-type ~fact-record))))
+        decl-rules (for [{:keys [decl-name
+                                 fact-type
+                                 fact-record]} facts]
+                     `(defrule ~decl-name
+                        [~(:c fact-type)]
+                        [~(:c fact-record)]
+                        =>
+                        (println (str "class:" ~n ~fact-type ~fact-record))))]
+    `(do
+       ~@decl-rules
+       (vector
+        ~@fact-rules))))
 
 (comment
-  (mk-types 5000)
+  (clear-ns-productions!)
+  (mk-types 2500)
   (def rules
-    (mk-rules 5000))
+    (mk-rules 2500))
   (keys @session-cache)
   (when-let [v (first (.cache ^clojure.core.cache.SoftCache @compiler-cache))]
     (.getValue v))
@@ -68,9 +80,9 @@
   (count (.cache ^clojure.core.cache.SoftCache @compiler-cache))
 
   (time
-   (mk-session (conj rules {:ns-name (ns-name *ns*)
-                            :lhs [{:type :foobar14
-                                   :constraints []}]
-                            :rhs `(println ~(str :foobar))})
+   (mk-session 'user [(conj rules {:ns-name (ns-name *ns*)
+                                   :lhs [{:type :foobar16
+                                          :constraints []}]
+                                   :rhs `(println ~(str :foobar))})]
                :cache session-cache
                :compiler-cache compiler-cache)))
